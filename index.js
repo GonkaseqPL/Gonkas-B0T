878566471e34a001078b196d01ad7539cf52d125
GonkaseqPL <32412160+GonkaseqPL@users.noreply.github.com>
2018-05-06 12:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-clientInformation.onLine('ready', ();
-console.log('Gotowy!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-        message.reply('pong');
-    }
-    ");
-    
-    //THIS MUST BE THIS WAY"
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.NDQxOTYyMDc4MTA2OTQzNDk5.DdBqzQ.XPpb_JRddq_9Vzb96brh5cB1VyM);
