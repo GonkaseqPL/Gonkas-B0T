98516356ee01a86bfa1008701041b2ecdaaff31b
GonkaseqPL <32412160+GonkaseqPL@users.noreply.github.com>
2018-05-06 12:31:58 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const discord = require('discord.js');
+const bot = new Discord.Client();
+
+clientInformation.onLine('ready', ();
+console.log('Gotowy!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+    ");
+    
+    //THIS MUST BE THIS WAY"
+client.login(process.env.NDQxOTYyMDc4MTA2OTQzNDk5.DdBqzQ.XPpb_JRddq_9Vzb96brh5cB1VyM);
